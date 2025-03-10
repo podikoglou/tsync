f563a7eac0c90587df101b32433056013486aa68
Alex <alex.podikoglou@gmail.com>
2025-03-10 17:10:58 +0200

    refactor: use postcard

--- src/proto.rs
+++ src/proto.rs
@@ -1,11 +1,8 @@
-use std::{
-    io::{BufRead, Write},
-    str::FromStr,
-};
-
-use anyhow::bail;
-
 use crate::header::Header;
+use anyhow::bail;
+use byteorder::ByteOrder;
+use postcard::{from_bytes, to_allocvec};
+use std::io::{BufRead, Write};
 
 #[derive(Debug)]
 pub struct FileMetadata {
@@ -21,30 +18,57 @@ pub struct Piece {
     // TODO: checksum!
 }
 
-pub fn write_file_metadata<W: Write>(writer: &mut W, file: &FileMetadata) -> anyhow::Result<()> {
-    writer.write(Header::Name(file.name.clone()).to_string().as_bytes())?;
-    writer.write(Header::Pieces(file.pieces_amount).to_string().as_bytes())?;
+macro_rules! write_struct {
+    ($writer: expr, $struct: expr) => {
+        let serialized = to_allocvec(&$struct)?;
 
-    dbg!(Header::Pieces(file.pieces_amount).to_string());
+        // todo: maybe we can do it using the byteorder crate just inc ase
+        $writer.write(&serialized.len().to_le_bytes())?;
+        $writer.write(&serialized)?;
+    };
+}
+
+macro_rules! read_struct {
+    ($reader: expr, $buf: expr, $type: ty) => {{
+        // resize buffer to 8 bytes and read u32 (size)
+        $buf.resize(8, 0x00);
+        $reader.read_exact(&mut $buf)?;
+
+        let size = byteorder::LittleEndian::read_u32(&$buf);
+
+        // resize buffer to size, read struct data
+        $buf.resize(size as usize, 0x00);
+        $reader.read_exact(&mut $buf)?;
+
+        from_bytes::<$type>(&mut $buf)?
+    }};
+}
+
+pub fn write_file_metadata<W: Write>(writer: &mut W, file: &FileMetadata) -> anyhow::Result<()> {
+    let name_header = Header::Name(file.name.clone());
+    let pieces_header = Header::Pieces(file.pieces_amount);
+
+    write_struct!(writer, name_header);
+    write_struct!(writer, pieces_header);
 
     Ok(())
 }
 
 pub fn read_file_metadata<R: BufRead>(reader: &mut R) -> anyhow::Result<FileMetadata> {
-    let mut buf = String::default();
+    let mut buf: Vec<u8> = vec![0; 8];
 
-    // read name header
-    reader.read_line(&mut buf)?;
+    // read names header
+    let name_header = read_struct!(reader, buf, crate::proto::Header);
 
-    let name = match Header::from_str(&buf)? {
+    let name = match name_header {
         Header::Name(val) => val,
         other => bail!("header must be name header, not {:?}", other),
     };
 
-    // read pieces header
-    reader.read_line(&mut buf)?;
+    // read pieces amount header
+    let pieces_amount_header = read_struct!(reader, buf, crate::proto::Header);
 
-    let pieces_amount = match Header::from_str(&buf)? {
+    let pieces_amount = match pieces_amount_header {
         Header::Pieces(val) => val,
         other => bail!("header must be pieces header, not {:?}", other),
     };
@@ -56,29 +80,30 @@ pub fn read_file_metadata<R: BufRead>(reader: &mut R) -> anyhow::Result<FileMeta
 }
 
 pub fn write_piece<W: Write>(writer: &mut W, piece: &Piece) -> anyhow::Result<()> {
-    writer.write(Header::Id(piece.id).to_string().as_bytes())?;
-    writer.write(Header::Size(piece.size).to_string().as_bytes())?;
+    let id_header = Header::Id(piece.id);
+    let size_header = Header::Size(piece.size);
 
-    writer.write(&piece.data)?;
+    write_struct!(writer, id_header);
+    write_struct!(writer, size_header);
 
     Ok(())
 }
 
 pub fn read_piece<R: BufRead>(reader: &mut R) -> anyhow::Result<Piece> {
-    let mut header_buf = String::default();
+    let mut buf: Vec<u8> = vec![0; 8];
 
     // read id header
-    reader.read_line(&mut header_buf)?;
+    let id_header = read_struct!(reader, buf, crate::proto::Header);
 
-    let id = match Header::from_str(&header_buf)? {
+    let id = match id_header {
         Header::Id(val) => val,
         other => bail!("header must be id header, not {:?}", other),
     };
 
     // read size header
-    reader.read_line(&mut header_buf)?;
+    let size_header = read_struct!(reader, buf, crate::proto::Header);
 
-    let size = match Header::from_str(&header_buf)? {
+    let size = match size_header {
         Header::Size(val) => val,
         other => bail!("header must be size header, not {:?}", other),
     };
